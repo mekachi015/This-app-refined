b94a3dfee7d22fac0804e447c70218ded1bba0c2
mekachi015 <katlegomakoti@gmail.com>
2025-11-10 18:17:52 +0200

    frontend

---
+++ Backend/demo/src/main/java/com/This_App_Backend/demo/ThisAppApplication.java
@@ -0,0 +1,13 @@
+package com.This_App_Backend.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThisAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThisAppApplication.class, args);
+	}
+
+}
---
+++ Backend/demo/src/test/java/com/This_App_Backend/demo/ThisAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.This_App_Backend.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThisAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
